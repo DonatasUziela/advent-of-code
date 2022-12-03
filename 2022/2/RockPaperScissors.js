a8e65f0efac85d2100728ffd9465ef0636bcfa5a
Donatas Uziela <4706298+DonatasUziela@users.noreply.github.com>
2022-12-03 13:59:24 +0200

    refactor challenge number 2

--- 2022/2/RockPaperScissors.js
+++ 2022/2/RockPaperScissors.js
@@ -2,85 +2,44 @@ const fs = require('fs');
 const path = require('path');
 
 const input = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'utf-8');
-const pairs = input.split('\n').map(l => l.split(' '));
+const plays = input.split('\n');
 
-const [OPPONENT_ROCK, OPPONENT_PAPER, OPPONENT_SCISSORS] = ['A', 'B', 'C'];
-const [MY_ROCK, MY_PAPER, MY_SCISSORS]  = ['X','Y','Z'];
-
-const winingCombinations = {
-    [OPPONENT_ROCK]: MY_PAPER,
-    [OPPONENT_PAPER]: MY_SCISSORS,
-    [OPPONENT_SCISSORS]: MY_ROCK
-};
-const drawCombinations = {
-    [OPPONENT_ROCK]: MY_ROCK,
-    [OPPONENT_PAPER]: MY_PAPER,
-    [OPPONENT_SCISSORS]: MY_SCISSORS
-};
-const loosingCombintations = {
-    [OPPONENT_ROCK]: MY_SCISSORS,
-    [OPPONENT_PAPER]: MY_ROCK,
-    [OPPONENT_SCISSORS]: MY_PAPER
+const scoreByPlay = {
+    'A X': 4,
+    'A Y': 8,
+    'A Z': 3,
+    'B X': 1,
+    'B Y': 5,
+    'B Z': 9,
+    'C X': 7,
+    'C Y': 2,
+    'C Z': 6,
 }
 
-const win = (opponentShape) => winingCombinations[opponentShape];
-const draw = (opponentShape) => drawCombinations[opponentShape];
-const loose = (opponentShape) => loosingCombintations[opponentShape];
+const result3 = plays
+    .map(p => scoreByPlay[p])
+    .reduce((sum, s) => sum += s)
 
-const scoreByShape = {
-    [MY_ROCK]: 1,
-    [MY_PAPER]: 2,
-    [MY_SCISSORS]: 3
-};
-
-// Part 1
-
-const wonGamesScore = pairs
-    .filter(([opponentShape, myShape]) => win(opponentShape) === myShape)
-    .length * 6;
-
-const drawGamesScore = pairs
-    .filter(([opponentShape, myShape]) => draw(opponentShape) === myShape)
-    .length * 3;
-
-const shapeScore = pairs
-    .map(([, myShape]) => scoreByShape[myShape])
-    .reduce((result, score) => result += score)
-
-const totalScore = wonGamesScore + drawGamesScore + shapeScore;
-
-console.log({ wonGamesScore, drawGamesScore, shapeScore, totalScore })
-
-// totalScore: 13526
+console.log(result3) // 13526
 
 // Part 2
 
-const wonGamesScore2 = pairs
-    .filter(([, result]) => result === 'Z')
-    .length * 6;
-
-const drawGamesScore2 = pairs
-    .filter(([, result]) => result === 'Y')
-    .length * 3;
-
-const strategyBySymbol = {
-    X: loose,
-    Y: draw,
-    Z: win
+const scoreByPlay2 = {
+    'A X': 3,
+    'A Y': 4,
+    'A Z': 8,
+    'B X': 1,
+    'B Y': 5,
+    'B Z': 9,
+    'C X': 2,
+    'C Y': 6,
+    'C Z': 7,
 }
 
-const shapeScore2 = pairs
-    .map(([opponentShape, symbol]) => {
-        const strategy = strategyBySymbol[symbol];
-        const myShape = strategy(opponentShape);
-        return scoreByShape[myShape]
-    })
-    .reduce((result, score) => result += score);
+const result4 = plays
+    .map(p => scoreByPlay2[p])
+    .reduce((sum, s) => sum += s)
 
-const totalScore2 = wonGamesScore2 + drawGamesScore2 + shapeScore2;
-
-console.log({ wonGamesScore2, drawGamesScore2, shapeScore2, totalScore2 })
-
-// totalScore2: 14204
+console.log(result4) // 14204
 
 // node 2022/2/RockPaperScissors.js
