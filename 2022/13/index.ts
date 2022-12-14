e81ab96ac056d0ade918e3f22fbabb1fb40df1e1
Donatas Uziela <4706298+DonatasUziela@users.noreply.github.com>
2022-12-14 17:29:00 +0200

    typescript

--- 2022/13/index.ts
+++ 2022/13/index.ts
@@ -1,16 +1,14 @@
-const { expect } = require('chai');
-const fs = require('fs');
-const { chunk, sum } = require('lodash');
-const path = require('path');
+import { expect } from 'chai';
+import { readFileSync } from 'fs';
+import { chunk, sum } from 'lodash';
+import { resolve }  from 'path';
 
-const taskInput = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'utf-8');
-const testData = fs.readFileSync(path.resolve(__dirname, 'testData.txt'), 'utf-8');
+const taskInput = readFileSync(resolve(__dirname, 'input.txt'), 'utf-8');
+const testData = readFileSync(resolve(__dirname, 'testData.txt'), 'utf-8');
 
-/**
- * @param {(number|number[])[]} left
- * @param {(number|number[])[]} right
- */
-const compare = (left, right, depth = 0) => {
+type Value = number | number[]
+
+const compare = (left: Value[], right: Value[]): boolean | undefined => {
     for (let i = 0; i < Math.max(left.length, right.length); i++) {
         const [l, r] = [left[i], right[i]];
 
@@ -24,18 +22,18 @@ const compare = (left, right, depth = 0) => {
         if (typeof l !== 'undefined' && typeof r === 'undefined') return false
 
         if (l instanceof Array && r instanceof Array) {
-            const res = compare(l, r, depth++);
+            const res = compare(l, r);
             if (typeof res === 'boolean') return res;
             continue
         }
 
         if (l instanceof Array && typeof r === 'number') {
-            const res = compare(l, [r], depth++);
+            const res = compare(l, [r]);
             if (typeof res === 'boolean') return res;
             continue
         }
         if (typeof l === 'number' && r instanceof Array) {
-            const res = compare([l], r, depth++);
+            const res = compare([l], r);
             if (typeof res === 'boolean') return res;
             continue
         }
@@ -46,23 +44,17 @@ const compare = (left, right, depth = 0) => {
     return
 }
 
-/**
- * @param {string} input
- */
-const solve = (input) => {
+const solve = (input: string) => {
     const pairs = chunk(
         input
             .split('\n')
             .filter(r => r)
-            .map(r => JSON.parse(r)), 2
+            .map(r => JSON.parse(r) as Value[]),
+        2
     );
 
-    const compared = pairs
+    const indexesInOrder = pairs
         .map(([left, right]) => compare(left, right))
-
-    // console.log(compared)
-
-    const indexesInOrder = compared
         .map((result, index) => result ? index + 1 : result)
         .filter(i => i)
 
@@ -74,10 +66,7 @@ expect(solve(taskInput)).to.equal(6086)
 
 // Part 2
 
-/**
- * @param {string} input
- */
-const solve2 = (input) => {
+const solve2 = (input: string) => {
     const DIVIDER_1 = [[2]];
     const DIVIDER_2 = [[6]];
 
@@ -98,4 +87,4 @@ const solve2 = (input) => {
 expect(solve2(testData)).to.equal(140)
 expect(solve2(taskInput)).to.equal(27930);
 
-// node 2022/13/index.js 
+// npx ts-node 2022/13/index.ts
