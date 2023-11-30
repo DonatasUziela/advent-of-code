d718d57ad4b75caa5ee000ffb72822248efd7cf3
Donatas Uziela <4706298+DonatasUziela@users.noreply.github.com>
2023-11-30 22:54:07 +0200

    create 2023 folder

---
+++ 2023/1/index.ts
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const input = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'utf-8');
+const entries = input.split('\n');
+
+// Part 1
+
+console.log({ input });
+
+// Part 2
+
+
+// npx ts-node 2023/1/index.ts
