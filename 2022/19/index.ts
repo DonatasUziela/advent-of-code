0a74adb6869e5c533b7a62023dcd79a3d5e23e23
Donatas Uziela <4706298+DonatasUziela@users.noreply.github.com>
2023-12-02 22:05:34 +0200

    some more linting

--- 2022/19/index.ts
+++ 2022/19/index.ts
@@ -1,131 +1,130 @@
-import { expect } from 'chai';
-import { readFileSync } from 'fs';
-import { resolve } from 'path';
+import { expect } from 'chai'
+import { readFileSync } from 'fs'
+import { resolve } from 'path'
 
-const taskInput = readFileSync(resolve(__dirname, 'input.txt'), 'utf-8');
-const testData = readFileSync(resolve(__dirname, 'testData.txt'), 'utf-8');
+const taskInput = readFileSync(resolve(__dirname, 'input.txt'), 'utf-8')
+const testData = readFileSync(resolve(__dirname, 'testData.txt'), 'utf-8')
 
 type ResourceType = 'ore' | 'clay' | 'obsidian' | 'geode'
 type Resources = Record<ResourceType, number>
-type Cost = Partial<Record<ResourceType, number>>;
+type Cost = Partial<Record<ResourceType, number>>
 type RobotCosts = Record<ResourceType, Cost>
-type Robots = Record<ResourceType, number>;
+type Robots = Record<ResourceType, number>
 
 interface BluePrint extends RobotCosts {
-    id: number;
+  id: number
 }
 
 const extractCost = (input: string) => input
-    .split('costs ')[1]
-    .split('and ')
-    .reduce<Cost>((cost, l) => {
-        const [amount, resource] = l.split(' ');
-        cost[resource as ResourceType] = parseInt(amount);
-        return cost;
-    }, {})
+  .split('costs ')[1]
+  .split('and ')
+  .reduce<Cost>((cost, l) => {
+  const [amount, resource] = l.split(' ')
+  cost[resource as ResourceType] = parseInt(amount)
+  return cost
+}, {})
 
 const parseBlueprints = (input: string): BluePrint[] => input
-    .split('\r\n')
-    .map<BluePrint>(blueprint => {
-        const [ore, clay, obsidian, geode] = blueprint
-            .split('.')
-            .slice(0, -1)
-            .map(extractCost);
+  .split('\r\n')
+  .map<BluePrint>(blueprint => {
+  const [ore, clay, obsidian, geode] = blueprint
+    .split('.')
+    .slice(0, -1)
+    .map(extractCost)
 
-        return {
-            id: parseInt(blueprint
-                .split(':')[0]
-                .split(' ')
-                .pop() || '0'
-            ),
-            ore,
-            clay,
-            obsidian,
-            geode
-        };
-    })
+  return {
+    id: parseInt(blueprint
+      .split(':')[0]
+      .split(' ')
+      .pop() ?? '0'
+    ),
+    ore,
+    clay,
+    obsidian,
+    geode
+  }
+})
 
 const canAfford = (cost: Cost, resources: Resources) => Object.entries(cost)
-    .every(([resource, amount]) =>
-        resources[resource as ResourceType] >= amount
-    )
+  .every(([resource, amount]) =>
+    resources[resource as ResourceType] >= amount
+  )
 
 const deductImmutable = (cost: Cost, resources: Resources) => {
-    const newResources = { ...resources }
-    Object.entries(cost).forEach(([resource, amount]) => {
-        newResources[resource as ResourceType] -= amount
-    })
-    return newResources
+  const newResources = { ...resources }
+  Object.entries(cost).forEach(([resource, amount]) => {
+    newResources[resource as ResourceType] -= amount
+  })
+  return newResources
 }
 
 const decide = (blueprint: BluePrint, resources: Resources, alwaysBuyObsidian: boolean): ResourceType | undefined => {
-    if (canAfford(blueprint.geode, resources)) {
-        return 'geode'
-    }
+  if (canAfford(blueprint.geode, resources)) {
+    return 'geode'
+  }
 
-    if (canAfford(blueprint.obsidian, resources) && (alwaysBuyObsidian || (Math.random() > 0.2))) {
-        return 'obsidian'
-    }
+  if (canAfford(blueprint.obsidian, resources) && (alwaysBuyObsidian || (Math.random() > 0.2))) {
+    return 'obsidian'
+  }
 
-    if (canAfford(blueprint.clay, resources) && Math.random() > 0.5) {
-        return 'clay'
-    }
+  if (canAfford(blueprint.clay, resources) && Math.random() > 0.5) {
+    return 'clay'
+  }
 
-    if (canAfford(blueprint.ore, resources) && Math.random() > 0.5) {
-        return 'ore'
-    }
+  if (canAfford(blueprint.ore, resources) && Math.random() > 0.5) {
+    return 'ore'
+  }
 }
 
-
 const addResources = (resources: Resources, robots: Robots) => {
-    const newResources = { ...resources }
-    Object.entries(robots).forEach(([resource, count]) => {
-        if (count > 0) {
-            newResources[resource as ResourceType] += count;
-        }
-    })
-    return newResources;
+  const newResources = { ...resources }
+  Object.entries(robots).forEach(([resource, count]) => {
+    if (count > 0) {
+      newResources[resource as ResourceType] += count
+    }
+  })
+  return newResources
 }
 
 const takeTurn = (turn: number, blueprint: BluePrint, resources: Resources, robots: Robots, maxMinutes: number, alwaysBuyObsidian: boolean): number => {
-    let newRobots = robots;
-    let newResources = resources;
-    const type = decide(blueprint, resources, alwaysBuyObsidian)
+  let newRobots = robots
+  let newResources = resources
+  const type = decide(blueprint, resources, alwaysBuyObsidian)
 
-    if (type) {
-        newRobots = {
-            ...robots,
-            [type]: robots[type] + 1
-        }
-        newResources = deductImmutable(blueprint[type], resources);
+  if (type) {
+    newRobots = {
+      ...robots,
+      [type]: robots[type] + 1
     }
+    newResources = deductImmutable(blueprint[type], resources)
+  }
 
-    newResources = addResources(newResources, robots);
+  newResources = addResources(newResources, robots)
 
-    if (turn === maxMinutes) return newResources.geode;
+  if (turn === maxMinutes) return newResources.geode
 
-    return takeTurn(turn + 1, blueprint, newResources, newRobots, maxMinutes, alwaysBuyObsidian);
+  return takeTurn(turn + 1, blueprint, newResources, newRobots, maxMinutes, alwaysBuyObsidian)
 }
 
 const solveBlueprintMultipleTimes = (blueprint: BluePrint, maxMinutes: number, maxTimes: number, alwaysBuyObsidian: boolean) => {
-    const initialResources = { ore: 0, clay: 0, obsidian: 0, geode: 0 };
-    const initialRobots = { ore: 1, clay: 0, obsidian: 0, geode: 0 }
-    let bestResult = 0;
-    for (let i = 0; i < maxTimes; i++) {
-        let result = takeTurn(1, blueprint, initialResources, initialRobots, maxMinutes, alwaysBuyObsidian);
-        bestResult = Math.max(result, bestResult)
-    }
-    console.log({ id: blueprint.id, bestResult })
+  const initialResources = { ore: 0, clay: 0, obsidian: 0, geode: 0 }
+  const initialRobots = { ore: 1, clay: 0, obsidian: 0, geode: 0 }
+  let bestResult = 0
+  for (let i = 0; i < maxTimes; i++) {
+    const result = takeTurn(1, blueprint, initialResources, initialRobots, maxMinutes, alwaysBuyObsidian)
+    bestResult = Math.max(result, bestResult)
+  }
+  console.log({ id: blueprint.id, bestResult })
 
-    return bestResult
+  return bestResult
 }
 
 const solve = (input: string) => {
-    return parseBlueprints(input)
-        .map(blueprint => {
-            return blueprint.id * solveBlueprintMultipleTimes(blueprint, 24, 10000, true)
-        } )
-        .reduce((sum, item) => sum + item)
+  return parseBlueprints(input)
+    .map(blueprint => {
+      return blueprint.id * solveBlueprintMultipleTimes(blueprint, 24, 10000, true)
+    })
+    .reduce((sum, item) => sum + item)
 }
 
 expect(solve(testData)).to.equal(33)
@@ -134,15 +133,15 @@ expect(solve(taskInput)).to.equal(1382)
 // Part 2
 
 const solve2 = (input: string) => {
-    return parseBlueprints(input)
-        .slice(0, 3)
-        .map(blueprint => {
-            return solveBlueprintMultipleTimes(blueprint, 32, 400000, false)
-        } )
-        .reduce((sum, item) => sum * item)
+  return parseBlueprints(input)
+    .slice(0, 3)
+    .map(blueprint => {
+      return solveBlueprintMultipleTimes(blueprint, 32, 400000, false)
+    })
+    .reduce((sum, item) => sum * item)
 }
 
 expect(solve2(testData)).to.equal(3472)
-expect(solve2(taskInput)).to.equal(31740);
+expect(solve2(taskInput)).to.equal(31740)
 
 // npx ts-node 2022/19/index.ts
--- 2022/20/index.ts
+++ 2022/20/index.ts
@@ -1,47 +1,45 @@
-import { expect } from "chai";
-import { readFileSync } from "fs";
-import { resolve } from "path";
+import { expect } from 'chai'
+import { readFileSync } from 'fs'
+import { resolve } from 'path'
 
-const testData = readFileSync(resolve(__dirname, "testData.txt"), "utf-8");
-const taskInput = readFileSync(resolve(__dirname, "input.txt"), "utf-8");
+const testData = readFileSync(resolve(__dirname, 'testData.txt'), 'utf-8')
+const taskInput = readFileSync(resolve(__dirname, 'input.txt'), 'utf-8')
 
 const parseInput = (input: string) =>
-  input.split("\n").map((n) => parseInt(n, 10));
+  input.split('\n').map((n) => parseInt(n, 10))
 
 const move = (
-  numbers: { value: number; originalIndex: number }[],
+  numbers: Array<{ value: number, originalIndex: number }>,
   index: number,
   log = false
 ) => {
-  const len = numbers.length;
+  const len = numbers.length
 
   const currentIndex = numbers.findIndex(
     ({ originalIndex }) => originalIndex === index
-  );
-  if (currentIndex === -1) throw `Number with originalIndex ${index} not found`;
-  const numberToMove = numbers[currentIndex].value;
+  )
+  if (currentIndex === -1) throw new Error(`Number with originalIndex ${index} not found`)
+  const numberToMove = numbers[currentIndex].value
 
-  const toMove = numberToMove % (len - 1);
-  const toMove2 = currentIndex + toMove;
+  const toMove = numberToMove % (len - 1)
+  const toMove2 = currentIndex + toMove
   const toMove3 =
-    toMove2 < 0
-      ? // wrap from start to end
-        len + toMove2 - 1
-      : toMove2 >= len
-      ? // wrap from end to start
-        toMove2 % (len - 1)
-      : toMove2;
+    toMove2 < 0 // wrap from start to end
+      ? len + toMove2 - 1
+      : toMove2 >= len // wrap from end to start
+        ? toMove2 % (len - 1)
+        : toMove2
 
-  const newIndex = toMove3;
+  const newIndex = toMove3
 
   if (newIndex === index) {
     // console.log(`${numberToMove} does not move:`);
-    return numbers;
+    return numbers
   }
 
-  const newNumbers = numbers.slice(0);
-  const [removed] = newNumbers.splice(currentIndex, 1);
-  newNumbers.splice(newIndex, 0, removed);
+  const newNumbers = numbers.slice(0)
+  const [removed] = newNumbers.splice(currentIndex, 1)
+  newNumbers.splice(newIndex, 0, removed)
 
   if (log) {
     // console.log(
@@ -51,108 +49,108 @@ const move = (
     // );
   }
 
-  return newNumbers;
-};
+  return newNumbers
+}
 
 const test = () => {
   const initialArrangement = [1, 2, -3, 3, -2, 0, 4].map(
     (value, originalIndex) => ({ value, originalIndex })
-  );
+  )
 
-  const result1 = move(initialArrangement, 0);
-  expect(result1.map(({ value }) => value)).to.deep.eq([2, 1, -3, 3, -2, 0, 4]);
+  const result1 = move(initialArrangement, 0)
+  expect(result1.map(({ value }) => value)).to.deep.eq([2, 1, -3, 3, -2, 0, 4])
 
-  const result2 = move(result1, 1);
-  expect(result2.map(({ value }) => value)).to.deep.eq([1, -3, 2, 3, -2, 0, 4]);
+  const result2 = move(result1, 1)
+  expect(result2.map(({ value }) => value)).to.deep.eq([1, -3, 2, 3, -2, 0, 4])
 
-  const result3 = move(result2, 2);
-  expect(result3.map(({ value }) => value)).to.deep.eq([1, 2, 3, -2, -3, 0, 4]);
-};
+  const result3 = move(result2, 2)
+  expect(result3.map(({ value }) => value)).to.deep.eq([1, 2, 3, -2, -3, 0, 4])
+}
 
-test();
+test()
 
 const solve = (input: string, log = false) => {
-  const numbers = parseInput(input);
+  const numbers = parseInput(input)
   const initialData = numbers.map((value, originalIndex) => ({
     value,
-    originalIndex,
-  }));
+    originalIndex
+  }))
 
   if (log) {
-    console.log("Initial arrangement:");
-    console.log(numbers.join(", "));
+    console.log('Initial arrangement:')
+    console.log(numbers.join(', '))
   }
 
-  let result = initialData;
+  let result = initialData
   for (let i = 0; i < numbers.length; i++) {
-    result = move(result, i, log);
-    if (log) console.log(result.map(({ value }) => value).join(", "));
+    result = move(result, i, log)
+    if (log) console.log(result.map(({ value }) => value).join(', '))
   }
 
-  const indexOfZero = result.findIndex(({ value }) => value === 0);
+  const indexOfZero = result.findIndex(({ value }) => value === 0)
 
   const findNth = (n: number) => {
-    const toMove = n % numbers.length;
-    const indexOfFoundNumber = (indexOfZero + toMove) % numbers.length;
-    return result[indexOfFoundNumber];
-  };
+    const toMove = n % numbers.length
+    const indexOfFoundNumber = (indexOfZero + toMove) % numbers.length
+    return result[indexOfFoundNumber]
+  }
 
-  const coord1 = findNth(1000).value;
-  const coord2 = findNth(2000).value;
-  const coord3 = findNth(3000).value;
+  const coord1 = findNth(1000).value
+  const coord2 = findNth(2000).value
+  const coord3 = findNth(3000).value
 
-  if (log) console.log({ coord1, coord2, coord3 });
+  if (log) console.log({ coord1, coord2, coord3 })
 
-  return coord1 + coord2 + coord3;
-};
+  return coord1 + coord2 + coord3
+}
 
-expect(solve(testData)).to.equal(3);
-expect(solve(taskInput)).to.equal(2827);
+expect(solve(testData)).to.equal(3)
+expect(solve(taskInput)).to.equal(2827)
 
 // Part 2
 
 const solve2 = (input: string, log = false) => {
-  const numbers = parseInput(input);
-  const decryptionKey = 811589153;
+  const numbers = parseInput(input)
+  const decryptionKey = 811589153
   const initialData = numbers.map((value, originalIndex) => ({
     value: value * decryptionKey,
-    originalIndex,
-  }));
+    originalIndex
+  }))
 
   if (log) {
-    console.log("Initial arrangement:");
-    console.log(initialData.map(({ value }) => value).join(", "));
+    console.log('Initial arrangement:')
+    console.log(initialData.map(({ value }) => value).join(', '))
   }
 
-  let result = initialData;
+  let result = initialData
   for (let mixingRound = 1; mixingRound <= 10; mixingRound++) {
     for (let i = 0; i < numbers.length; i++) {
-      result = move(result, i, log);
+      result = move(result, i, log)
     }
     if (log) {
-      console.log(`After ${mixingRound} of mixing:`);
-      console.log(result.map(({ value }) => value).join(", "));
+      console.log(`After ${mixingRound} of mixing:`)
+      console.log(result.map(({ value }) => value).join(', '))
     }
   }
 
-  const indexOfZero = result.findIndex(({ value }) => value === 0);
+  const indexOfZero = result.findIndex(({ value }) => value === 0)
 
   const findNth = (n: number) => {
-    const toMove = n % numbers.length;
-    const indexOfFoundNumber = (indexOfZero + toMove) % numbers.length;
-    return result[indexOfFoundNumber];
-  };
+    const toMove = n % numbers.length
+    const indexOfFoundNumber = (indexOfZero + toMove) % numbers.length
+    return result[indexOfFoundNumber]
+  }
 
-  const coord1 = findNth(1000).value;
-  const coord2 = findNth(2000).value;
-  const coord3 = findNth(3000).value;
+  const coord1 = findNth(1000).value
+  const coord2 = findNth(2000).value
+  const coord3 = findNth(3000).value
 
-  if (log) console.log({ coord1, coord2, coord3 });
+  if (log) console.log({ coord1, coord2, coord3 })
 
-  return coord1 + coord2 + coord3;
-};
+  return coord1 + coord2 + coord3
+}
 
-expect(solve2(testData, true)).to.equal(1623178306);
-expect(solve2(taskInput)).to.equal(undefined);
+expect(solve2(testData, true)).to.equal(1623178306)
+expect(solve2(taskInput)).to.equal(undefined)
 
 // npx ts-node 2022/20/index.ts
