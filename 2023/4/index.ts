238c256b6bf33d53cdb02298212f8ef6f825a93d
Donatas Uziela <4706298+DonatasUziela@users.noreply.github.com>
2023-12-04 08:06:27 +0200

    2024/4

--- 2023/4/index.ts
+++ 2023/4/index.ts
@@ -12,31 +12,30 @@ interface Card {
   winningNumbers: number[]
 }
 
-const parse = (input: string): Card[] => {
-  const cards = input
-    .split('\r\n')
-    .map(c => c.split(':').pop()!)
-    .map(n => n.split('|'))
-    .map(n => n.map(i => i
-      .split(' ')
-      .filter(s => s)
-      .map(s => parseInt(s, 10))
-    ))
-    .map((p, index) => ({
-      index: index + 1,
-      winningNumbers: p[0],
-      cardNumbers: p[1]
-    }))
-
-  return cards
-}
+const parse = (input: string): Card[] => input
+  .split('\r\n')
+  .map(c => c.split(':').pop()!)
+  .map(n => n.split('|'))
+  .map(n => n.map(i => i
+    .split(' ')
+    .filter(s => s)
+    .map(s => parseInt(s, 10))
+  ))
+  .map((p, index) => ({
+    index: index + 1,
+    winningNumbers: p[0],
+    cardNumbers: p[1]
+  }))
 
 const solve = (input: string) => {
   const parsed = parse(input)
-  const myWinningNumbers = parsed
-    .map(({ cardNumbers, winningNumbers }) => cardNumbers
-      .filter(n => winningNumbers.includes(n)))
-  const points = myWinningNumbers.map(n => n.length ? Math.pow(2, n.length - 1) : 0)
+
+  const myWinningNumbers = parsed.map(({ cardNumbers, winningNumbers }) =>
+    cardNumbers.filter(n =>
+      winningNumbers.includes(n)))
+
+  const points = myWinningNumbers.map(n =>
+    n.length ? Math.pow(2, n.length - 1) : 0)
 
   return sum(points)
 }
@@ -51,23 +50,26 @@ const solve2 = (input: string) => {
   const originalCards = parsed
     .map(({ cardNumbers, winningNumbers, index }) => ({
       index,
-      winners: cardNumbers.filter(n => winningNumbers.includes(n)).length
+      wonCardsCount: cardNumbers.filter(n => winningNumbers.includes(n)).length
     }))
 
   const cardCounts: Record<number, number | undefined> = {}
 
   const increment = (index: number) => {
-    cardCounts[index] = (cardCounts[index] ?? 0) + 1
+    const newValue = (cardCounts[index] ?? 0) + 1
+    cardCounts[index] = newValue
+    return newValue
   }
 
-  originalCards.forEach(({ index, winners }) => {
-    increment(index)
-    const copies = cardCounts[index] ?? 0
+  originalCards.forEach(({ index: originalCardIndex, wonCardsCount }) => {
+    const copies = increment(originalCardIndex)
 
-    console.log({ index, copies, winners })
     for (let c = 0; c < copies; c++) {
-      for (let i = 1; i <= winners; i++) {
-        const wonCardIndex = index + i
+      for (
+        let wonCardIndex = originalCardIndex + 1;
+        wonCardIndex <= originalCardIndex + wonCardsCount;
+        wonCardIndex++
+      ) {
         increment(wonCardIndex)
       }
     }
