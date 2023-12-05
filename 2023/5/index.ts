62e85b49670310fb804e184652c85e1974b0176f
Donatas Uziela <4706298+DonatasUziela@users.noreply.github.com>
2023-12-05 09:49:21 +0200

    2023/05

--- 2023/5/index.ts
+++ 2023/5/index.ts
@@ -64,28 +64,6 @@ const mapSeedToLocation = (seed: number, mapsByType: ResourcesMapsByType) => {
   return current
 }
 
-const mapLocationToSeed = (location: number, mapsByType: ResourcesMapsByType) => {
-  let current = location
-
-  for (let typeIndex = mapsByType.length - 1; typeIndex >= 0; typeIndex--) {
-    const maps = mapsByType[typeIndex]
-    let found = false
-
-    for (let mapIndex = 0; mapIndex < maps.length; mapIndex++) {
-      const { source, destination, length } = maps[mapIndex]
-
-      if (destination <= current && current <= destination + length - 1) {
-        current = source + (current - destination)
-        found = true
-        break
-      }
-    }
-    if (!found) return
-  }
-
-  return current
-}
-
 const solve = (input: string) => {
   const { seeds, maps } = parse(input)
 
@@ -102,48 +80,20 @@ expect(solve(taskInput)).to.equal(57075758)
 const solve2 = (input: string) => {
   const { seeds, maps } = parse(input)
 
-  const isValidSeed = (seed: number) => {
-    for (let pairIndex = 0; pairIndex < seeds.length; pairIndex += 2) {
-      const start = seeds[pairIndex]
-      const length = seeds[pairIndex + 1]
-      if (start <= seed && seed <= start + length - 1) return true
-    }
-    return false
-  }
-
-  const locationsAscending = generateValidLocations(maps[maps.length - 1])
-
-  for (const location of locationsAscending) {
-    const pontetialSeed = mapLocationToSeed(location, maps)
-    console.log({ pontetialSeed })
-    if (pontetialSeed && isValidSeed(pontetialSeed)) {
-      return location
+  let lowestLocation = Number.MAX_SAFE_INTEGER
+  for (let pairIndex = 0; pairIndex < seeds.length; pairIndex += 2) {
+    const start = seeds[pairIndex]
+    const length = seeds[pairIndex + 1]
+    console.log({ start, length })
+    for (let seed = start; seed < start + length; seed++) {
+      const location = mapSeedToLocation(seed, maps)
+      lowestLocation = Math.min(location, lowestLocation)
     }
   }
-
-// let lowestLocation = Number.MAX_SAFE_INTEGER
-//   for (let pairIndex = 0; pairIndex < seeds.length; pairIndex += 2) {
-//     const start = seeds[pairIndex]
-//     const length = seeds[pairIndex + 1]
-//     console.log({ start, length })
-//     for (let seed = start; seed < start + length; seed++) {
-//       const location = mapSeedToLocation(seed, maps)
-//       lowestLocation = Math.min(location, lowestLocation)
-//     }
-//   }
-//   return lowestLocation
+  return lowestLocation
 }
 
 expect(solve2(testData)).to.equal(46)
-// expect(solve2(taskInput)).to.equal(undefined)
+expect(solve2(taskInput)).to.equal(31161857)
 
-function generateValidLocations (locationMaps: ResourcesMap[]): number[] {
-  const locations: number[] = []
-  for (const { destination, length } of locationMaps) {
-    for (let location = destination; location < destination + length - 1; location++) {
-      locations.push(location)
-    }
-  }
-  return locations.sort((a, b) => a - b)
-}
 // npx ts-node 2023/5/index.ts
