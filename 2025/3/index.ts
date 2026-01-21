5cea7349306ac8be05ef12166294f581fc3246d6
Donatas Uziela <donatasu@wix.com>
2026-01-21 13:51:32 +0200

    2025 3/2

--- 2025/3/index.ts
+++ 2025/3/index.ts
@@ -6,33 +6,48 @@ import { resolve } from 'path'
 const taskInput = readFileSync(resolve(__dirname, 'input.txt'), 'utf-8')
 const testData = readFileSync(resolve(__dirname, 'testData.txt'), 'utf-8')
 
-const parse = (input: string) =>
-  input
-    .split('\n')
-    // .map(line => line.split(''))
+const parse = (input: string) => input.split('\n')
 
-const solve = (input: string) => {
-  const banks = parse(input)
+const findMax = (bank: string) => {
+  const numbers = bank.split('').map(b => parseInt(b, 10))
+  const max = Math.max(...numbers)
+  const index = numbers.indexOf(max)
+  return {
+    max,
+    index
+  }
+}
+
+const getJoltage = (banks: string[], batteryCount: number) => {
   const joltages = banks.map(bank => {
-    const numbers = bank.slice(0, -1).split('').map(b => parseInt(b, 10))
-    const max = Math.max(...numbers)
-    const firstIndex = numbers.indexOf(max)
-    const remainingBankNumbers = bank.slice(firstIndex + 1).split('').map(b => parseInt(b, 10))
-    const secondMax = Math.max(...remainingBankNumbers)
-    return Number(`${max}${secondMax}`)
+    const turnedOnBatteries: number[] = []
+    let scannedBank = bank
+    for (let i = 1; i <= batteryCount; i++) {
+      const { max, index } = findMax(scannedBank.slice(0, (-batteryCount + i) || undefined))
+      scannedBank = scannedBank.slice(index + 1)
+      turnedOnBatteries.push(max)
+    }
+    return Number(turnedOnBatteries.join(''))
   })
   return sum(joltages)
 }
 
+const solve = (input: string) => {
+  const banks = parse(input)
+  return getJoltage(banks, 2)
+}
+
 expect(solve(testData)).to.equal(357)
-expect(solve(taskInput)).to.equal(undefined)
+expect(solve(taskInput)).to.equal(17403)
 
 // Part 2
 
 const solve2 = (input: string) => {
+  const banks = parse(input)
+  return getJoltage(banks, 12)
 }
 
-expect(solve2(testData)).to.equal(undefined)
-expect(solve2(taskInput)).to.equal(undefined)
+expect(solve2(testData)).to.equal(3121910778619)
+expect(solve2(taskInput)).to.equal(173416889848394)
 
 // npx ts-node 2025/3/index.ts
