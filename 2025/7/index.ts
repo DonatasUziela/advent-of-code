393cde7046c53099e6de07d1222eeca384bd63e6
Donatas Uziela <donatasu@wix.com>
2026-01-22 11:04:02 +0200

    2025 7/1

--- 2025/7/index.ts
+++ 2025/7/index.ts
@@ -11,11 +11,26 @@ const parse = (input: string): string[][] =>
     .map(line => line.split(''))
 
 const solve = (input: string) => {
-  const parsed = parse(input)
-  console.log(parsed)
+  const grid = parse(input)
+  const start = { x: grid[0].indexOf('S'), y: 0 }
+  const beamsMap = { [start.x]: true }
+  let splitCount = 0
+
+  for (let y = 1; y < grid.length; y++) {
+    const beamsXes = Object.keys(beamsMap).map(Number)
+    for (const beamX of beamsXes) {
+      if (grid[y][beamX] === '^' && beamsMap[beamX]) {
+        beamsMap[beamX] = false
+        beamsMap[beamX - 1] = true
+        beamsMap[beamX + 1] = true
+        splitCount++
+      }
+    }
+  }
+  return splitCount
 }
 
-expect(solve(testData)).to.equal(undefined)
+expect(solve(testData)).to.equal(21)
 expect(solve(taskInput)).to.equal(undefined)
 
 // Part 2
@@ -26,4 +41,4 @@ const solve2 = (input: string) => {
 expect(solve2(testData)).to.equal(undefined)
 expect(solve2(taskInput)).to.equal(undefined)
 
-// npx ts-node 2025/5/index.ts
+// npx ts-node 2025/7/index.ts
